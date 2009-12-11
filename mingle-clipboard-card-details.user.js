ac929942776e9d5d9c790b2435254260bc3249aa
Larry Karnowski <larry.karnowski@gmail.com>
2009-12-10 23:07:01 -0500

    Initial commit.

---
+++ mingle-clipboard-card-details.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name           Mingle Cut & Paste: Card Details Page
+// @namespace      http://karnowski.github.com/greasemonkey
+// @description    Adds a button to quickly copy the Mingle card's number & title to your clipboard.
+// @include        https://mingle.thinkrelevance.com/projects/zeus/cards/*
+// @require        http://code.jquery.com/jquery-latest.min.js
+// ==/UserScript==
+
+
+(function($){
+  $(function(){
+    var cardNumber = $("#card-index").text().trim();
+    var cardName = $("#card-short-description").text().trim().split("&nbsp;")[0];
+    var text = cardNumber + ": " + cardName;
+
+    var html = '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="110" height="14" id="clippy">\
+      <param name="movie" value="http://github.com/mojombo/clippy/raw/7329b72360100b9484bc6cc9f097ea35f08421ad/build/clippy.swf"/>\
+      <param name="allowScriptAccess" value="always"/>\
+      <param name="quality" value="high"/>\
+      <param name="scale" value="noscale"/>\
+      <param NAME="FlashVars" value="text=' + text +  '">\
+      <param name="bgcolor" value="#FFF2E5">\
+      <embed src="http://github.com/mojombo/clippy/raw/7329b72360100b9484bc6cc9f097ea35f08421ad/build/clippy.swf" width="110" height="14" name="clippy" quality="high" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" bgcolor="#FFF2E5"\
+           FlashVars="text=' + text + '"\
+       />\
+    </object>';
+
+    $(html).appendTo("#card-short-description");
+  });
+})(jQuery);

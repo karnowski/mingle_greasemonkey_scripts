1ca1de832ca1617824e87161e900ecd17ba0ca1f
Larry Karnowski <larry.karnowski@gmail.com>
2009-12-11 14:13:35 -0500

    WIP: for grabbing selected cards

--- mingle-clipboard-card-list.user.js
+++ mingle-clipboard-card-list.user.js
@@ -9,23 +9,25 @@
 
 (function($){
   $(function(){
-    // var cardNumber = $("#card-index").text().trim();
-    // var cardName = $("#card-short-description").text().trim().split("&nbsp;")[0];
-    // var text = cardNumber + ": " + cardName;
+    $('<a href="#" id="copy-selected-cards-to-clipboard-link">Copy</a>').appendTo(".for-selected-actions");
+    
+    $("#copy-selected-cards-to-clipboard-link").click(function(event){
+      event.preventDefault();
+      
+      var text = "";
+      
+      $("tr.card-row").each(function(){
+        var checked = $(this).find("input[type=checkbox]:checked").length;
 
-    // var html = '<object classid="clsid:d27cdb6e-ae6d-11cf-96b8-444553540000" width="110" height="14" id="clippy">\
-    //   <param name="movie" value="http://github.com/mojombo/clippy/raw/7329b72360100b9484bc6cc9f097ea35f08421ad/build/clippy.swf"/>\
-    //   <param name="allowScriptAccess" value="always"/>\
-    //   <param name="quality" value="high"/>\
-    //   <param name="scale" value="noscale"/>\
-    //   <param NAME="FlashVars" value="text=' + text +  '">\
-    //   <param name="bgcolor" value="#FFF2E5">\
-    //   <embed src="http://github.com/mojombo/clippy/raw/7329b72360100b9484bc6cc9f097ea35f08421ad/build/clippy.swf" width="110" height="14" name="clippy" quality="high" allowScriptAccess="always" type="application/x-shockwave-flash" pluginspage="http://www.macromedia.com/go/getflashplayer" bgcolor="#FFF2E5"\
-    //        FlashVars="text=' + text + '"\
-    //    />\
-    // </object>';
-
-    // $(html).appendTo("#card-short-description");
-    $("<div> Hey, I'm doing something!</div>").appendTo(".for-selected-actions");
+        if(checked > 0) {
+          var cardNumber = $(this).find("td.number").text();
+          var cardName = $(this).find("td.card-name").text();
+          text += "#" + cardNumber + ": " + cardName + "\n";
+        }
+      });
+      
+      alert(text);
+    });
+    
   });
 })(jQuery);
